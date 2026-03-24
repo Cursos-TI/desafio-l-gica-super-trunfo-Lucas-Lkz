9c5ad1d7fa7f29f70f90a456bb6d93e82a75b2a8
Lucas-Lkz <202603326561@alunos.estacio.br>
2026-03-24 08:00:32 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int opcao;
+    int opcao, opcao2;
+    float valor1_C1, valor1_C2;
+    float valor2_C1, valor2_C2;
+
 //variaveis carta1
     
     char estado1[3], codigo1[5], cidade1[50];
@@ -114,56 +117,323 @@ int main(){
 
    //if (populacao1 > populacao2){
     //printf("Carta1 venceu!\n");
-   //}else {
+   //}else{ //
     //printf("Carta2 venceu!\n");
 
-    printf("Menu de Comparação: \n");
-    printf("1. População \n");
-    printf("2. Área \n");
-    printf("3. PIB \n");
-    printf("4. Pontos turísticos \n");
-    printf("5. Densidade Demográfica \n");
-    scanf("%d", &opcao);
+    //Comparação interativa
+    int escolha1, escolha2;
 
-    switch (opcao)
-    {
-    case 1:
-        if (populacao1 > populacao2){
-            printf("A Carta vencedora é: %s\n", cidade1);
-        } else {
-            printf("A carta vencedora é: %s", cidade2);
-        } 
+    //Primeiro menu
+    printf("### Escolha o PRIMEIRO Atributo ###\n");
+    printf("1. População\n");
+    printf("2. Pontos Turísticos\n");
+    printf("3. Área\n");
+    printf("4. PIB\n");
+    printf("5. Densidade\n");
+    printf("6. PIB per Capita\n");
+    scanf("%d", &escolha1);
+
+    switch(escolha1) {
+        case 1: //População
+          valor1_C1 = (float)populacao1;
+          valor1_C2 = (float)populacao2;
+        printf("Atributo 1: População\n");
+        printf("Agora escolha o segundo atributo (2,3,4,5,6): \n");
+        scanf("%d", &escolha2);
+             
+             switch (escolha2) //menu interno para escolha do 2º atributo
+             {
+             case 2:
+                valor2_C1 = (float)turisticos1;
+                valor2_C2 = (float)turisticos2;
+                printf("Segundo atributo: Pontos Turísticos\n");
+                break;
+            
+            case 3:
+                valor2_C1 = area1;
+                valor2_C2 = area2;
+                printf("Segundo atributo: Área\n");
+                break;
+            
+            case 4:
+                valor2_C1 = pib1;
+                valor2_C2 = pib2;
+                printf("Segundo atributo: PIB\n");
+                break;
+            
+            case 5:
+                valor2_C1 = (1.0 / densidade1);
+                valor2_C2 = (1.0 / densidade2);
+                printf("Segundo atributo: Densidade\n");
+                break;
+            
+            case 6:
+                valor2_C1 = capita1;
+                valor2_C2 = capita2;
+                printf("Segundo atributo: PIB per Capita\n");
+                break;
+             
+             default:
+                printf("Opção inválida para o segundo atributo\n");
+                return 0; //encerra o programa quando escolhe errado
+                break;
+             }
+        break;
+        
+        case 2: //pontos turísticos primeiro
+          valor1_C1 = (float)turisticos1;
+          valor1_C2 = (float)turisticos2;
+          printf("Atributo 1: Pontos Turísticos\n");
+          printf("Agora escolha o segundo atributo: (1,3,4,5,6)\n");
+          scanf("%d", &escolha2);
+            
+            switch (escolha2)
+            {
+            case 1:
+                valor2_C1 = (float)populacao1;
+                valor2_C2 = (float)populacao2;
+                printf("Segundo atributo: População\n");
+                break;
+            case 3:
+                valor2_C1 = area1;
+                valor2_C2 = area2;
+                printf("Segundo atributo: Área\n");
+                break;
+            case 4:
+                valor2_C1 = pib1;
+                valor2_C2 = pib2;
+                printf("Segundo atributo: PIB\n");
+                break;
+            case 5:
+                valor2_C1 = (1.0 / densidade1);
+                valor2_C2 = (1.0 / densidade2);
+                printf("Segundo atributo: Densidade\n");
+                break;
+            case 6:
+                valor2_C1 = capita1;
+                valor2_C2 = capita2;
+                printf("Segundo atributo: PIB per Capita\n");
+                break;
+            
+            default:
+                printf("Opção inválida para o segundo atributo\n");
+                return 0; //encerra o programa quando escolhe errado
+                break;
+            }
+        break;
+
+        case 3: //Área primeiro
+           valor1_C1 = area1;
+           valor1_C2 = area2;
+           printf("Atributo 1: Área\n");
+           printf("Agora escolha o segundo atributo (1,2,4,5,6)\n");
+           scanf("%d", &escolha2);
+
+           switch (escolha2)
+           {
+           case 1:
+            valor2_C1 = (float)populacao1;
+            valor2_C2 = (float)populacao2;
+            printf("Segundo atributo: População\n");
+            break;
+           
+            case 2:
+            valor2_C1 = (float)turisticos1;
+            valor2_C2 = (float)turisticos2;
+            printf("Segundo atributo: Pontos Turísticos\n");
+            break;
+           
+            case 4:
+             valor2_C1 = pib1;
+             valor2_C2 = pib2;
+             printf("Segundo valor: PIB\n");
+             break;
+           
+            case 5:
+                valor2_C1 = (1.0 / densidade1);
+                valor2_C2 = (1.0 / densidade2);
+                printf("Segundo atributo: Densidade\n");
+                break;
+            case 6:
+                valor2_C1 = capita1;
+                valor2_C2 = capita2;
+                printf("Segundo atributo: PIB per Capita\n");
+                break;
+            
+            default:
+                printf("Opção inválida para o segundo atributo\n");
+                return 0; //encerra o programa quando escolhe errado
+                break; 
+           
+           }
+        
+        break;
+
+        case 4: //Escolha pib primeiro
+           valor1_C1 = pib1;
+           valor1_C2 = pib2;
+           printf("Atributo 1: PIB\n");
+           printf("Agora escolha o segundo atributo (1,2,3,5,6):\n");
+           scanf("%d", &escolha2);
+
+           switch (escolha2)
+           {
+           case 1:
+            valor2_C1 = (float)populacao1;
+            valor2_C2 = (float)populacao2;
+            printf("Segundo atributo: População\n");
+            break;
+           
+            case 2:
+             valor2_C1 = (float)turisticos1;
+             valor2_C2 = (float)turisticos2;
+             printf("Segundo atributo: Pontos Turísticos\n");
+             break;
+            
+            case 3:
+              valor2_C1 = area1;
+              valor2_C2 = area2;
+              printf("Segundo atributo: Área\n");
+              break;
+
+            case 5:
+              valor2_C1 = (1.0 / densidade1);
+              valor2_C2 = (1.0 / densidade2);
+              printf("Segundo atributo: Densidade\n");
+              break;
+
+            case 6:
+              valor2_C1 = capita1;
+              valor2_C2 = capita2;
+              printf("Segundo atributo: PIB per Capita\n");
+              break;
+
+           default:
+             printf("Opção inválida para o segundo atributo.\n");
+             return 0;
+             break;
+           }
+        break;
+
+        case 5: //escolha densidade primeiro
+          valor1_C1 = (1.0 / densidade1);
+          valor1_C2 = (1.0 / densidade2);
+          printf("Atributo 1: Densidade\n");
+          printf("Agora escolha o segundo atributo (1,2,3,4,6): \n");
+          scanf("%d", &escolha2);
+
+          switch (escolha2)
+          {
+          case 1:
+            valor2_C1 = (float)populacao1;
+            valor2_C2 = (float)populacao2;
+            printf("Segundo atributo: População\n");
+            break;
+
+          case 2:
+            valor2_C1 = (float)turisticos1;
+            valor2_C2 = (float)turisticos2;
+            printf("Segundo atributo: Pontos Turísticos\n");
+            break;
+          
+          case 3:
+            valor2_C1 = area1;
+            valor2_C2 = area2;
+            printf("Segundo atributo: Área\n");
+            break;
+          
+          case 4:
+            valor2_C1 = pib1;
+            valor2_C2 = pib2;
+            printf("Segundo valor: PIB\n");
+            break;
+        
+          case 6:
+            valor2_C1 = capita1;
+            valor2_C2 = capita2;
+            printf("Segundo atributo: PIB per Capita\n");
+            break;
+          
+          default:
+            printf("Opção inválida para o segundo atributo.\n");
+            return 0;
+            break;
+          }
+
         break;
     
-    case 2:
-        if (area1 > area2) {
-            printf("A carta vencedora é: %s\n", cidade1);
-        } else {
-            printf("A carta vencedora é: %s\n", cidade2);
-        }
+        case 6: //escolha pib per capita primeiro
+           valor1_C1 = capita1;
+           valor1_C2 = capita2;
+           printf("Atributo1: PIB per Capita\n");
+           printf("Agora escolha o segundo atributo(1,2,3,4,5): \n");
+           scanf("%d", &escolha2);
+
+           switch (escolha2)
+           {
+           case 1:
+            valor2_C1 = (float)populacao1;
+            valor2_C2 = (float)populacao2;
+            printf("Segundo atributo: População\n");
+            break;
+          
+           case 2:
+            valor2_C1 = (float)turisticos1;
+            valor2_C2 = (float)turisticos2;
+            printf("Segundo atributo: Pontos Turísticos\n");
+            break;
+          
+           case 3:
+            valor2_C1 = area1;
+            valor2_C2 = area2;
+            printf("Segundo atributo: Área\n");
+            break;
+           
+           case 4:
+            valor2_C1 = pib1;
+            valor2_C2 = pib2;
+            printf("Segundo valor: PIB\n");
+            break;
+            
+           case 5:
+            valor2_C1 = (1.0 / densidade1);
+            valor2_C2 = (1.0 / densidade2);
+            printf("Segundo atributo: Densidade\n");
+            break;
+
+           default:
+            printf("Escolha inválida para o segundo atributo.\n");
+            return 0;
+            break;
+           }
+        
         break;
-    case 3:
-        if (pib1 > pib2) {
-            printf("A carta vencedora é: %s\n", cidade1);
-        } else {
-            printf("A carta vencedora é: %s\n", cidade2);
-        }
-        break;
-    case 4:
-        if (turisticos1 > turisticos2) {
-            printf("A carta vencedora é: %s\n", cidade1);
-        } else {
-            printf("A carta vencedora é: %s\n", cidade2);
-        }   
-        break;
-    case 5:
-        if (densidade1 < densidade2) {
-            printf("A carta vencedora é: %s\n", cidade1);
-        } else {
-            printf("A carta vencedora é: %s\n", cidade2);
-        }
-        break;
-    default:
-        printf("Opção inválida, selecione uma opção entre 1 e 5\n");
+
+        
     }
+
+    // Lógica de comparação
+
+    float soma1 = valor1_C1 + valor2_C1; 
+    float soma2 = valor1_C2 + valor2_C2;
+
+    printf("\n=================================\n");
+    printf("       RESULTADO DO DUELO     \n");
+    printf("===================================\n");
+    printf("%s: %.2fpontos\n", cidade1, soma1); //exibe a soma para C1
+    printf("%s: %.2fpontos\n", cidade2, soma2); //exibe a soma para C2
+    printf("-----------------------------------\n");
+
+    if(soma1 > soma2) {
+        printf("VENCEDOR: %s!\n", cidade1);
+    } else if (soma2 > soma1) {
+        printf("VENCEDOR: %s!\n", cidade2);    
+    } else {
+        printf("EMPATE TÉCNICO\n");
     }
+
+    printf("===============================\n");
+
+    return 0;
+  
+ }
